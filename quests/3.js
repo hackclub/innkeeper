e85f9c1f5d83d962643bd24ec6453b53c954efed
David M <62346025+aboutdavid@users.noreply.github.com>
2024-10-28 17:48:50 -0400

    stage 4 emergency fix

--- quests/3.js
+++ quests/3.js
@@ -2,7 +2,15 @@
  * @param {{app: import('@slack/bolt').App, prisma: import('@prisma/client').PrismaClient}}} param1
  */
 module.exports = async function ({ app, prisma, command, body, ack, respond }) {
-    if (body?.channel_id !== "C078Q8PBD4G" || body?.channel?.id !== "C078Q8PBD4G") await respond("Check <#C078Q8PBD4G> for the next quest!")
+    if (body?.channel_id !== "C078Q8PBD4G" || body?.channel?.id !== "C078Q8PBD4G") return await respond("Check <#C078Q8PBD4G> for the next quest!")
+    await prisma.user.update({
+        where: {
+            id: body?.user_id || body?.user?.id
+        },
+        data: {
+            stage: 4
+        }
+    })
     await app.client.chat.postEphemeral({
         channel: "C078Q8PBD4G",
         user: body?.user_id || body?.user?.id,
@@ -78,15 +86,7 @@ Example: /setuserlocation Atlanta, Georgia.` : (await sls.text())}`
                 }
             ]
         })
-        if (body.channel_id == "C078Q8PBD4G")
-            await prisma.user.update({
-                where: {
-                    id: body?.user_id || body?.user?.id
-                },
-                data: {
-                    stage: 4
-                }
-            })
+
 
     }, 2000)
 }
