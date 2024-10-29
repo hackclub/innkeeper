10f7ae160bd82838aa66cc7a0adde48b21f52a24
David M <62346025+aboutdavid@users.noreply.github.com>
2024-10-28 22:21:52 -0400

    make uris normal

--- quests/7.js
+++ quests/7.js
@@ -4,13 +4,14 @@
 const { metrics } = require('../utils/metrics')
 module.exports = async function ({ app, prisma, command, body, ack, respond }) {
     metrics.increment('events.flow.finish', 1)
+    const userId = body?.user_id || body?.user?.id
     await respond({
         blocks: [
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": `One last thing, you should set your <https://forms.fillout.com/t/dpjQEYgRa1us?id=${body?.user_id || body?.user?.id}|channel suggestion prefrences here>. You'll also be able to run \`/like\` on channels that you like, just like instagram (and get recommendations using \`/recommendations\`). Learn more in the <#C07SREWJ4DA> channel.
+                    "text": `One last thing, you should set your <https://forms.fillout.com/t/dpjQEYgRa1us?id=${userId.toUpperCase()}|channel suggestion prefrences here>. You'll also be able to run \`/like\` on channels that you like, just like instagram (and get recommendations using \`/recommendations\`). Learn more in the <#C07SREWJ4DA> channel.
 
 Congrats! You've done all of the quests.`
                 }
